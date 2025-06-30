9403b71097e5a9c5c238d63b51419b27acdb93e3
Ram Krishna Yadav <itsramky234@gmail.com>
2025-07-01 00:20:57 +0545

    Backend initialized

---
+++ backend1/src/index.ts
@@ -0,0 +1,10 @@
+import { WebSocketServer } from "ws";
+const wss = new WebSocketServer({ port: 8000 })
+
+wss.on("connection", function connection(ws) {
+    ws.on("error", console.error);
+    ws.on("message", function message(data) {
+        console.log("Received: ", data)
+    })
+    ws.send("something")
+})
