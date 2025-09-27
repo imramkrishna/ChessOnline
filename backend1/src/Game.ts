7c60d278c725adc2dca4d907b4f0a6999c82299c
Ram Krishna Yadav <itsramky234@gmail.com>
2025-09-27 21:35:30 +0545

    added moves histoy on backend

--- backend1/src/Game.ts
+++ backend1/src/Game.ts
@@ -1,18 +1,20 @@
 import { WebSocket } from "ws";
 import { Chess } from "chess.js"
 import { GAME_OVER, INIT_GAME, MOVE } from "./messages";
-import { Redis } from '@upstash/redis';
-
 interface moveType {
     from: string;
     to: string
 }
-
+interface Moves{
+    player: WebSocket,
+    moveTime: Date,
+    move: moveType
+}
 export class Game {
     public player1: WebSocket
     public player2: WebSocket
     private board: Chess
-    private moves: string[]
+    private moves: Moves[]
     private startTime: Date
     constructor(player1: WebSocket, player2: WebSocket) {
         this.player1 = player1
@@ -38,6 +40,7 @@ export class Game {
         try {
             console.log("Making move:", move);
             game.board.move(move)
+
         } catch (e) {
             console.error("Invalid move attempted:", move);
             return;
@@ -51,34 +54,51 @@ export class Game {
             return;
         }
         if (this.board.moves.length % 2 == 0) {
+             let newMove={
+                player:this.player1,
+                moveTime:new Date(),
+                move:move
+            }
+            game.moves.push(newMove)
             this.player1.send(JSON.stringify({
                 type: MOVE,
                 move: move,
                 board: this.board.fen(),
-                turn: this.board.turn()
+                turn: this.board.turn(),
+                AllMoves:game.moves
             }));
             this.player2.send(JSON.stringify({
                 type: MOVE,
                 move: move,
                 board: this.board.fen(),
-                turn: this.board.turn()
+                turn: this.board.turn(),
+                AllMoves:game.moves
             }));
+           
         }
         else {
+            let newMove={
+                player:this.player2,
+                moveTime:new Date(),
+                move:move
+            }
+            game.moves.push(newMove)
             this.player2.send(JSON.stringify({
                 type: MOVE,
                 payload: move,
                 board: this.board.fen(),
-                turn: this.board.turn()
+                turn: this.board.turn(),
+                AllMoves:game.moves
             }));
             this.player1.send(JSON.stringify({
                 type: MOVE,
                 payload: move,
                 board: this.board.fen(),
-                turn: this.board.turn()
+                turn: this.board.turn(),
+                AllMoves:game.moves
             }));
+            
         }
-        this.moves.push(JSON.stringify(move));
     }
 
 }
